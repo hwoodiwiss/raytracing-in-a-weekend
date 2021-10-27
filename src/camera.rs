009c4be804093ef4fda6a3b57833e71ba9916cd8
Hugo Woodiwiss <hj.woodiwiss@gmail.com>
2021-10-27 17:48:16 +0100

    Include image for png output Move to using camera structure for handling ray casts

---
+++ src/camera.rs
@@ -0,0 +1,26 @@
+use crate::{ray::Ray, vec3::Vec3};
+
+pub struct Camera {
+    origin: Vec3,
+    lower_left_corner: Vec3,
+    horizontal: Vec3,
+    vertical: Vec3,
+}
+
+impl Camera {
+    pub fn new() -> Self {
+        Self {
+            lower_left_corner: Vec3::new(-2.0, -1.0, -1.0),
+            horizontal: Vec3::new(4.0, 0.0, 0.0),
+            vertical: Vec3::new(0.0, 2.0, 0.0),
+            origin: Vec3::new(0.0, 0.0, 0.0),
+        }
+    }
+
+    pub fn get_ray(&self, u: f32, v: f32) -> Ray {
+        Ray::new(
+            self.origin,
+            self.lower_left_corner + (u * self.horizontal) + (v * self.vertical),
+        )
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::rc::Rc;
 
+use camera::Camera;
 use hitable::Hitable;
+use image::png::PngEncoder;
 use ray::Ray;
 
 #[macro_use]
@@ -8,6 +10,7 @@ extern crate impl_ops;
 
 use crate::{hitable::HitableList, shapes::sphere::Sphere, vec3::Vec3};
 
+mod camera;
 mod hitable;
 mod ray;
 mod shapes;
@@ -16,7 +19,7 @@ mod vec3;
 fn ray_colour(ray: &Ray, hitable: &dyn Hitable) -> Vec3 {
     if let Some(hit) = hitable.hit(ray, 0.0, f32::MAX) {
         return 0.5
-            * Vec3::from_values(
+            * Vec3::new(
                 hit.normal.x() + 1.0,
                 hit.normal.y() + 1.0,
                 hit.normal.z() + 1.0,
@@ -24,38 +27,40 @@ fn ray_colour(ray: &Ray, hitable: &dyn Hitable) -> Vec3 {
     } else {
         let unit_direction = ray.direction.unit();
         let t = 0.5 * (unit_direction.y() + 1.0);
-        return &((1.0 - t) * &Vec3::from_values(1.0, 1.0, 1.0))
-            + &(t * &Vec3::from_values(0.5, 0.7, 1.0));
+        return &((1.0 - t) * &Vec3::new(1.0, 1.0, 1.0)) + &(t * &Vec3::new(0.5, 0.7, 1.0));
     };
 }
 
 fn main() {
     let nx = 2000;
     let ny = 1000;
-    let lower_left_corner = Vec3::from_values(-2.0, -1.0, -1.0);
-    let horizontal = Vec3::from_values(4.0, 0.0, 0.0);
-    let vertical = Vec3::from_values(0.0, 2.0, 0.0);
-    let origin = Vec3::from_values(0.0, 0.0, 0.0);
+
     let sphere_1: Box<Rc<dyn Hitable>> =
-        Box::new(Rc::new(Sphere::new(Vec3::from_values(0.0, 0.0, -1.0), 0.5)));
-    let sphere_2: Box<Rc<dyn Hitable>> = Box::new(Rc::new(Sphere::new(
-        Vec3::from_values(0.0, -100.5, -1.0),
-        100.0,
-    )));
+        Box::new(Rc::new(Sphere::new(Vec3::new(0.0, 0.0, -1.0), 0.5)));
+    let sphere_2: Box<Rc<dyn Hitable>> =
+        Box::new(Rc::new(Sphere::new(Vec3::new(0.0, -100.5, -1.0), 100.0)));
 
     let world = HitableList::new(&[sphere_1, sphere_2]);
-    print!("P3\n{} {} \n255\n", nx, ny);
+    let camera = Camera::new();
+    let mut image_bytes = Vec::new();
     for j in (0..ny).rev() {
         for i in 0..nx {
             let u = i as f32 / nx as f32;
             let v = j as f32 / ny as f32;
-            let direction = lower_left_corner + (u * horizontal) + (v * vertical);
-            let ray = Ray::from_values(origin, direction);
+            let ray = camera.get_ray(u, v);
             let col = ray_colour(&ray, &world);
-            let ir = (255.99 * col.r()) as i32;
-            let ig = (255.99 * col.g()) as i32;
-            let ib = (255.99 * col.b()) as i32;
-            print!("{} {} {}\n", ir, ig, ib);
+            let ir = (255.99 * col.r()) as u16;
+            let ig = (255.99 * col.g()) as u16;
+            let ib = (255.99 * col.b()) as u16;
+            image_bytes.extend_from_slice(&ir.to_le_bytes());
+            image_bytes.extend_from_slice(&ig.to_le_bytes());
+            image_bytes.extend_from_slice(&ib.to_le_bytes());
         }
     }
+
+    let mut file = std::fs::File::create("raytracing.png").unwrap();
+    let png_encoder = PngEncoder::new(&mut file);
+    png_encoder
+        .encode(&image_bytes, nx, ny, image::ColorType::Rgb16)
+        .unwrap();
 }
