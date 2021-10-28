d2d12789fb7967273b34eb2735a3390e692773d3
Hugo Woodiwiss <hj.woodiwiss@gmail.com>
2021-10-28 08:52:40 +0100

    Implement diffuse material

---
+++ src/materials/diffuse.rs
@@ -0,0 +1,29 @@
+use crate::{
+    hitable::RayHit,
+    material::{Material, MaterialHit},
+    point_in_unit_sphere,
+    ray::Ray,
+    vec3::Vec3,
+};
+
+pub struct Diffuse {
+    albedo: Vec3,
+}
+
+impl Diffuse {
+    pub fn new(albedo: Vec3) -> Self {
+        Self { albedo }
+    }
+}
+
+impl Material for Diffuse {
+    fn scatter(&self, in_ray: &Ray, hit: &RayHit) -> Option<MaterialHit> {
+        let target = hit.point + hit.normal + point_in_unit_sphere();
+        let scatter_ray = Ray::new(hit.point, target);
+        let attenuation = self.albedo;
+        Some(MaterialHit {
+            attenuation,
+            scatter_ray,
+        })
+    }
+}
