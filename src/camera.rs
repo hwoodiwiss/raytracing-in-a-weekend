948968f3af9a70d5e523f09b2b0de1fd6094dab2
Hugo Woodiwiss <hj.woodiwiss@gmail.com>
2021-10-30 14:38:19 +0100

    Make camera position and target configurable

--- src/camera.rs
+++ src/camera.rs
@@ -10,15 +10,18 @@ pub struct Camera {
 }
 
 impl Camera {
-    pub fn new(vert_fov: f32, aspect: f32) -> Self {
+    pub fn new(position: Vec3, look_at: Vec3, up: Vec3, vert_fov: f32, aspect: f32) -> Self {
         let theta = vert_fov * consts::PI / 180.0;
         let half_height = f32::tan(theta / 2.0);
         let half_width = half_height * aspect;
+        let w = (position - look_at).unit();
+        let u = up.cross(&w).unit();
+        let v = w.cross(&u);
         Self {
-            lower_left_corner: Vec3::new(-half_width, -half_height, -1.0),
-            horizontal: Vec3::new(2.0 * half_width, 0.0, 0.0),
-            vertical: Vec3::new(0.0, 2.0 * half_height, 0.0),
-            origin: Vec3::new(0.0, 0.0, 0.0),
+            lower_left_corner: position - half_width * u - half_height * v - w,
+            horizontal: 2.0 * half_width * u,
+            vertical: 2.0 * half_height * v,
+            origin: position,
         }
     }
 
